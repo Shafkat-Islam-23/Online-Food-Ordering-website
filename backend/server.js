7c745f5110bbf596f18ce0f217afe8ae05dd6bfe
Shafkat Islam <shafkatislam.12@gmail.com>
2025-11-28 00:50:48 +0600

    added cart

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import { connectDB } from "./config/db.js";
 
 import userRouter from "./routes/userRoute.js";
 import "dotenv/config";
+import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
 
 //app config
 const app = express();
@@ -16,7 +18,10 @@ app.use(cors());
 //db connection
 connectDB();
 
+app.use("/images", express.static("uploads"));
 app.use("/api/user", userRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
 
 app.get("/", (req, res) => {
   res.send("API working");
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,12 @@ import React, { useState } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
-//import Cart from "./pages/Cart/Cart";
-//import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
+import Cart from "./pages/Cart/Cart";
+import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
 import Footer from "./components/Footer/Footer";
 import LoginPopup from "./components/LoginPopup/LoginPopup";
-//import Verify from "./pages/Verify/Verify";
-//import MyOrders from "./pages/MyOrders/MyOrders";
+import Verify from "./pages/Verify/Verify";
+import MyOrders from "./pages/MyOrders/MyOrders";
 
 const App = () => {
   const [showLogin, setShowLogin] = useState(false);
@@ -18,10 +18,10 @@ const App = () => {
         <Navbar setShowLogin={setShowLogin} />
         <Routes>
           <Route path="/" element={<Home />} />
-          {/* <Route path="/cart" element={<Cart />} /> */}
-          {/* <Route path='/order' element={<PlaceOrder/>}/>
-        <Route path='/verify' element={<Verify/>}/>
-        <Route path='/myorders' element={<MyOrders/>}/> */}
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/order" element={<PlaceOrder />} />
+          <Route path="/verify" element={<Verify />} />
+          <Route path="/myorders" element={<MyOrders />} />
         </Routes>
       </div>
       <Footer />
